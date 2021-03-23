1cf057f028c1a8020d8bf000b58e0ea57bf7ed20
Anthony RYCK <anthony.ryck@hotmail.fr>
2021-03-23 15:00:34 +0100

    Ajout du projet BlazorServerTemplateSqlite

---
+++ README.md
@@ -0,0 +1,27 @@
+# Project Templates for Visual Studio
+
+Il y aura plusieurs projet pour avoir la base pour la création de templates de projet pour Visual Studio.
+
+### Projets
+- **TemplateSqlite** : un projet Blazor Server avec une base de données SQLite et l'authentification "Comptes Individuel".
+
+
+
+### Choses communes
+
+- Les logs sont fait avec [Serilog](https://serilog.net/).
+- J'ai "Scaffolding" toutes les pages Identity.
+- La connexion (_Login_) se fait sur le "UserName" avec Identity, et non plus sur l'Email.
+- La base de données SQLite est créée au démarrage de l'application et est mise dans un répertoire "Database", pour simplifier dans le cas ou l'application serait dans un container.  
+- A la création de la base de données, un utilisateur "root" est injecté et il a le rôle "Admin"
+```csharp
+var poweruser = new IdentityUser
+                {
+                    UserName = "root",
+                    Email = "root@email.com",
+                    EmailConfirmed = true
+                };
+string userPwd = "Azerty123!";
+var createPowerUser = await userManager.CreateAsync(poweruser, userPwd);
+```
+- J'ai traduit les pages de Login, Register,... en français.
---
+++ src/TemplateSqlite/BlazorSqliteTemplate/Program.cs
@@ -0,0 +1,68 @@
+using BlazorSqliteTemplate.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.IO;
+
+namespace BlazorSqliteTemplate
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string pathLog = Path.Combine(AppContext.BaseDirectory, "Logs");
+			if (!Directory.Exists(pathLog))
+			{
+				Directory.CreateDirectory(pathLog);
+			}
+
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+				.MinimumLevel.Override("System", LogEventLevel.Warning)
+				.WriteTo.RollingFile(Path.Combine(pathLog, "log-{Date}.txt"))
+				.CreateLogger();
+
+			try
+			{
+				var host = CreateHostBuilder(args).Build();
+
+				// Création du répertoire pour la base de donnée
+				string pathDatabase = Path.Combine(AppContext.BaseDirectory, "database");
+				if (!Directory.Exists(pathDatabase))
+					Directory.CreateDirectory(pathDatabase);
+
+				var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+				using (var scope = scopeFactory.CreateScope())
+				{
+					var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+					var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+					var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+					// Vrai si la base de données est créée, false si elle existait déjà.
+					if (db.Database.EnsureCreated())
+					{
+						DataInitializer.InitData(roleManager, userManager).Wait();
+					}
+				}
+
+				host.Run();
+			}
+			catch (Exception ex)
+			{
+				Log.Error(ex, "Erreur dans Main");
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
