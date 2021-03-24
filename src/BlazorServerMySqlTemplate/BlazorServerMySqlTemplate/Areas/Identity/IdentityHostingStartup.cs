467eb595f32e6a435c4ad85f3d95e2a79f8014ab
Anthony RYCK <anthony.ryck@hotmail.fr>
2021-03-24 09:23:05 +0100

    Ajout du template Blazor Server pour MySQL.

---
+++ src/BlazorServerMySqlTemplate/BlazorServerMySqlTemplate/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,23 @@
+﻿using BlazorServerMySqlTemplate.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: HostingStartup(typeof(BlazorServerMySqlTemplate.Areas.Identity.IdentityHostingStartup))]
+
+namespace BlazorServerMySqlTemplate.Areas.Identity
+{
+	public class IdentityHostingStartup : IHostingStartup
+	{
+		public void Configure(IWebHostBuilder builder)
+		{
+			builder.ConfigureServices((context, services) =>
+			{
+			});
+		}
+	}
+}
